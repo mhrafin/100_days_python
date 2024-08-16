0ee29be181d3a040a57d5eda6901e1459b7170c6
rafin_pc <110781737+mhrafin@users.noreply.github.com>
2024-08-16 13:08:00 +0600

    Day 6 Initial Commit

---
+++ day6/maze.py
@@ -0,0 +1,20 @@
+# Used this code on this link
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    print(not at_goal())
+    if wall_on_right():
+        print(front_is_clear() and wall_on_right())
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+    else:
+        turn_right()
+        move()
