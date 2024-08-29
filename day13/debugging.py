c7da1c6a921895d1239aaa78f7d38710fa23192f
raflinhom <110781737+mhrafin@users.noreply.github.com>
2024-08-29 12:37:33 +0600

    Day 13 Initial Commit
    Also updated .gitignore to ignore vscode files.

---
+++ day13/debugging.py
@@ -0,0 +1,47 @@
+# def my_function():
+#     for i in range(1,20): #20 is not inclusive
+#         print(i)
+#         if i == 20:            
+#             print("You got it")
+
+# my_function()
+
+
+
+
+# def mutate(alist):
+#     blist =[]
+#     for item in alist:
+#         newitem = item * 2
+#     blist.append(newitem)
+#     print(blist)
+
+# mutate([1,2,3,5,8,13])
+
+
+# # Which year do you want to check?
+# year = int(input())
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+
+
+
+# target = int(input())
+# for number in range(1, target + 1):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+#   elif number % 3 == 0:
+#     print("Fizz")
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
