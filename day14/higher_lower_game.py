52366b71f4304539f0f240611108fc94753c066d
raflinhom <110781737+mhrafin@users.noreply.github.com>
2024-08-30 21:29:20 +0600

    Day 14 Initial Commit

---
+++ day14/higher_lower_game.py
@@ -0,0 +1,87 @@
+import random
+import textwrap
+import tv_show_list
+from os import system,name
+from sys import exit
+
+def main():
+    logo()
+    # Need a list of subjects with a metric to compare them
+    # Select two of them at Random
+    score = 0
+    item_a = random.choice(tv_show_list.tv_shows)
+    wrapped_description_a = textwrap.fill(item_a["description"])
+
+    item_b = random.choice(tv_show_list.tv_shows)
+    wrapped_description_b = textwrap.fill(item_b["description"])
+
+    while item_a["episodes"] == item_b["episodes"]:
+        item_b = random.choice(tv_show_list.tv_shows)
+        wrapped_description_b = textwrap.fill(item_b["description"])
+    
+    while True:
+        #Show items and ask
+        print(f"Compare A: \n{item_a['name']}, \n{wrapped_description_a}")
+        print(r'''
+ _    _ _______
+  \  /  |______
+   \/   ______|
+''')
+        print(f"Against B: \n{item_b['name']}, \n{wrapped_description_b}\n")
+        print("Does B has Higher or Lower no. of epidsodes than A.")
+        ans = input("Type 'Higher' or 'Lower'\n").lower()
+
+        if ans == "higher":
+            if item_a["episodes"] > item_b["episodes"]:
+                system("cls" if name == "nt" else "clear")
+
+                print("You Lost!")
+                print(f"Your Score is {score}.")
+                exit(0)
+            elif item_a["episodes"] < item_b["episodes"]:
+                system("cls" if name == "nt" else "clear")
+
+                print("You Win!")
+                item_a = item_b
+                while item_a["episodes"] == item_b["episodes"]:
+                    item_b = random.choice(tv_show_list.tv_shows)
+                    wrapped_description_b = textwrap.fill(item_b["description"])
+                score += 1
+                print(f"Your Score is {score}.")
+                continue
+        elif ans == "lower":
+            if item_a["episodes"] > item_b["episodes"]:
+                system("cls" if name == "nt" else "clear")
+
+                print("You Win")
+                item_a = item_b
+                while item_a["episodes"] == item_b["episodes"]:
+                    item_b = random.choice(tv_show_list.tv_shows)
+                    wrapped_description_b = textwrap.fill(item_b["description"])
+                score += 1
+                print(f"Your Score is {score}.")
+                continue
+            elif item_a["episodes"] < item_b["episodes"]:
+                system("cls" if name == "nt" else "clear")
+
+                print("You Lost!")
+                print(f"Your Score is {score}.")
+                exit(0)
+        else:
+            exit(0)
+
+ 
+def logo():
+    print(r'''
+ _     _ _____  ______ _     _ _______  ______
+ |_____|   |   |  ____ |_____| |______ |_____/
+ |     | __|__ |_____| |     | |______ |    \_
+                                              
+         _____  _  _  _ _______  ______       
+ |      |     | |  |  | |______ |_____/       
+ |_____ |_____| |__|__| |______ |    \_       
+''')
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ day14/tv_show_list.py
@@ -0,0 +1,52 @@
+tv_shows = [
+    {
+        "name" : "The Office(2005)",
+        "description" : "The show documents the exploits of a paper supply company in Scranton, Pennsylvania. With an office including the likes of various peers, this series takes a look at the lives of its co-workers.",
+        "episodes": 188
+    },
+    {
+        "name" : "Brooklyn Nine-Nine(2013)",
+        "description" : "Ray Holt, an eccentric commanding officer, and his diverse and quirky team of odd detectives solve crimes in Brooklyn, New York City.",
+        "episodes": 153
+    },
+    {
+        "name" : "Game of Thrones(2011)",
+        "description" : "Nine noble families wage war against each other in order to gain control over the mythical land of Westeros. Meanwhile, a force is rising after millenniums and threatens the existence of living men.",
+        "episodes": 74
+    },
+    {
+        "name" : "The Walking Dead(2010)",
+        "description" : "In the wake of a zombie apocalypse, various survivors struggle to stay alive. As they search for safety and evade the undead, they are forced to grapple with rival groups and difficult choices.",
+        "episodes": 177
+    },
+    {
+        "name" : "The Sopranos(1999)",
+        "description" : "Tony Soprano, an Italian-American mafia head based in New Jersey, struggles to manage his family and criminal life and confides his affairs to his psychiatrist Jennifer Melfi.",
+        "episodes": 86
+    },
+    {
+        "name" : "Breaking Bad(2008)",
+        "description" : "Walter White, a chemistry teacher, discovers that he has cancer and decides to get into the meth-making business to repay his medical debts. His priorities begin to change when he partners with Jesse.",
+        "episodes": 62
+    },
+    {
+        "name" : "The Umbrella Academy(2019)",
+        "description" : "A family of former child heroes, now grown apart, must reunite to continue to protect the world.",
+        "episodes": 36
+    },
+    {
+        "name" : "Dark(2017)",
+        "description" : "A family saga with a supernatural twist, set in a German town where the disappearance of two young children exposes the relationships among four families.",
+        "episodes": 26
+    },
+    {
+        "name" : "Avatar: The Last Airbender(2005)",
+        "description" : "In a war-torn world of elemental powers, a young boy reawakens to undertake a dangerous mystic quest to fulfill his destiny as the Avatar, and bring peace to the world.",
+        "episodes": 62
+    },
+    {
+        "name" : "Attack on Titan(2013)",
+        "description" : "After his hometown is destroyed, young Eren Jaeger vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.",
+        "episodes": 98
+    },
+]
