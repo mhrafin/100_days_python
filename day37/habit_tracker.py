47da098d237637c5386570e64b391be459bb862b
raflinhom <110781737+mhrafin@users.noreply.github.com>
2024-11-01 22:58:15 +0600

    Day 43 complete

--- day37/habit_tracker.py
+++ day37/habit_tracker.py
@@ -11,7 +11,7 @@ TODAY = str(datetime.now().date()).replace("-", "")
 
 PIXELA_TOKEN = os.getenv("PIXELA_TOKEN")
 
-GRAPH_ID = "graph1"
+GRAPH_ID = "graph3"
 USERNAME = "raf"
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -23,11 +23,10 @@ user_data = {
     "notMinor": "yes",
 }
 
-# new_user_res = requests.post(url=pixela_endpoint, json=new_user_data)
-# print(new_user_res.text)
+# new_user_res = requests.post(url=pixela_endpoint, json=user_data)
+# print(new_user_res.json())
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-print(graph_endpoint)
 
 graph_config = {
     "id": GRAPH_ID,
@@ -37,19 +36,21 @@ graph_config = {
     "color": "momiji",
 }
 
-token_header = {"X-USER-TOKEN": PIXELA_TOKEN}
+token_header = {"X-USER-TOKEN": "bad"}
 
-# graph_res = requests.post(url=graph_endpoint, json=graph_config, headers=graph_header)
-# print(graph_res.text)
+# graph_res = requests.post(url=graph_endpoint, json=graph_config, headers=token_header)
+# # graph_res.raise_for_status()
+# print(graph_res.json())
 
 post_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
-post_pixel_data = {"date": TODAY, "quantity": "1"}
+post_pixel_data = {"date": TODAY, "quantity": "4"}
 
 # post_pixel_res = requests.post(
 #     url=post_pixel_endpoint, json=post_pixel_data, headers=token_header
 # )
 # print(post_pixel_res.text)
+# print(post_pixel_res.url)
 
 upd_pixel_endp = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{TODAY}"
 
