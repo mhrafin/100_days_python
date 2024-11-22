702ac73858e440561e0ae36ddaf6871c4bdc5631
raflinhom <110781737+mhrafin@users.noreply.github.com>
2024-11-22 09:49:13 +0600

    Day 60 Complete

---
+++ day60/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    return render_template("login.html", username=username, password=password)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
